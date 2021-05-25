ffdeec55b9df7c26307b487c3856823287e288dc
Archi <JustArchi@JustArchi.net>
2021-05-25 19:56:05 +0200

    Update archi.sh

--- archi.sh
+++ archi.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
+#!/usr/bin/env sh
+set -eu
+
+cd "$(dirname "$(readlink -f "$0")")"
 
 git pull origin master
+
 rm -rf vendor
+
 composer install -n -o --no-dev --no-progress --no-suggest
